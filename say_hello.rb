52785613a13c79e61509ebcbb122bd90f4f9fae9
dynamic-router-3131 <jtboss47@gmail.com>
2019-06-16 14:36:17 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,12 +1,14 @@
 # Build your say_hello method here
 
 def say_hello(String name)
+  
   if (name == "")
     
     print "Hello Ruby Programmer!"
     else 
     print "Hello " + name + "!"
   end
+  
 end
 
 say_hello(Jon)
